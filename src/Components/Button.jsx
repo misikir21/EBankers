a146980639496403c568fc238f9d776a38d8b6ba
misikir21 <tekamisikir@gmail.com>
2023-10-19 06:20:46 -0700

    complete the business section

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-function Button() {
-  return <div>Button</div>;
+function Button({ styles }) {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 }
 
 export default Button;
