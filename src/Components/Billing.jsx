e3d78947ff476fee44ec9c9ee2f3d83e2b096927
misikir21 <tekamisikir@gmail.com>
2023-10-20 23:27:29 -0700

    add a billing section

--- src/Components/Billing.jsx
+++ src/Components/Billing.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 function Billing() {
   return (
-    <div>Billing</div>
-  )
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img
+          src={bill}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient " />
+
+        <div className="absolute z-[3] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient " />
+      </div>
+    </section>
+  );
 }
-export default Billing
+export default Billing;
