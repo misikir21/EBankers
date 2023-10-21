c72595b95ca9cb53ce2e476b85ede55c860735ae
misikir21 <tekamisikir@gmail.com>
2023-10-21 00:13:23 -0700

    start with card featue

--- src/Components/CardDeal.jsx
+++ src/Components/CardDeal.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 function CardDeal() {
-  return (
-    <div>CardDeal</div>
-  )
+  return <div>CardDeal</div>;
 }
 
-export default CardDeal
+export default CardDeal;
