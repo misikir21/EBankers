747fa9ca961f31d2e0d1d89b9cf4b80b8f265770
misikir21 <tekamisikir@gmail.com>
2023-10-21 00:23:21 -0700

    add carddeal featue

--- src/Components/CardDeal.jsx
+++ src/Components/CardDeal.jsx
@@ -3,7 +3,21 @@ import { card } from "../assets";
 import styles, { layout } from "../style";
 import Button from "./Button";
 function CardDeal() {
-  return <div>CardDeal</div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={`styles.paragraph} max-w-[470px] mt-5`}>
+          Find a better card deal
+          <br className="sm:block hidden" />
+          in few easy steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+          aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default CardDeal;
