a27adaadfc43877d557b9e6bc2b15cb659c20be4
misikir21 <tekamisikir@gmail.com>
2023-10-20 23:49:34 -0700

    add more styles to billings section

--- src/Components/Billing.jsx
+++ src/Components/Billing.jsx
@@ -13,6 +13,17 @@ function Billing() {
 
         <div className="absolute z-[3] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient " />
       </div>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="sm:block hidden" />
+          billing & invoicing.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+      </div>
     </section>
   );
 }
