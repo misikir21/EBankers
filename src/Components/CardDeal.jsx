4351a781820b216a517ca40fd4fcb51b3580172a
misikir21 <tekamisikir@gmail.com>
2023-10-21 00:32:27 -0700

    add left section of the card

--- src/Components/CardDeal.jsx
+++ src/Components/CardDeal.jsx
@@ -6,7 +6,7 @@ function CardDeal() {
   return (
     <section className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h2 className={`styles.paragraph} max-w-[470px] mt-5`}>
+        <h2 className={styles.heading2}>
           Find a better card deal
           <br className="sm:block hidden" />
           in few easy steps.
@@ -15,6 +15,7 @@ function CardDeal() {
           Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
           aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
         </p>
+        <Button styles="mt-10" />
       </div>
     </section>
   );
