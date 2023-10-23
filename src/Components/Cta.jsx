cf654a7ee3cebcb4de65618dc2a0b66b8531b956
misikir21 <tekamisikir@gmail.com>
2023-10-23 00:54:55 -0700

    add the cta feature

--- src/Components/Cta.jsx
+++ src/Components/Cta.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
-function Cta() {
-  return (
-    <div>Cta</div>
-  )
-}
-export default Cta
+import React from "react";
+import styles from "../style";
+import Button from "./Button";
+const Cta = () => {
+  <section
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding}
+     sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+  >
+    <div>
+      <h2>Let's try our service now</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus omnis
+        unde vero deserunt maiores debitis?s
+      </p>
+    </div>
+    <div>
+      <Button />
+    </div>
+  </section>;
+};
+export default Cta;
