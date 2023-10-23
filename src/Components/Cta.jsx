b50f22c61d381c354f55e0c836dac91725eb784e
misikir21 <tekamisikir@gmail.com>
2023-10-23 04:04:56 -0700

    fix cta issues

--- src/Components/Cta.jsx
+++ src/Components/Cta.jsx
@@ -1,21 +1,22 @@
-import React from "react";
 import styles from "../style";
 import Button from "./Button";
-const Cta = () => {
+
+const Cta = () => (
   <section
-    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding}
-     sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
   >
     <div className="flex-1 flex flex-col">
-      <h2 className={styles.heading2}>Let's try our service now</h2>
-      <p className={`${styles.paragraph}`}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus omnis
-        unde vero deserunt maiores debitis?s
+      <h2 className={styles.heading2}>Let’s try our service now!</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Everything you need to accept card payments and grow your business
+        anywhere on the planet.
       </p>
     </div>
+
     <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
       <Button />
     </div>
-  </section>;
-};
+  </section>
+);
+
 export default Cta;
