b1392b5eb0da860f25df448047b532453a251c5e
misikir21 <tekamisikir@gmail.com>
2023-10-24 00:01:43 -0700

    add a footer section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import styles, { logo } from "../style";
+import { footerLinks, socialMedia } from "../constants";
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div
+        className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}
+      ></div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
